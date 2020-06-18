4d203c40aeb71b4a1178477f52774f467c3799d3
iOS-Pradeep <pradeep.sagar.ios@gmail.com>
2020-06-18 23:42:57 +0530

    first commit

--- BindingExample/ContentView.swift
+++ BindingExample/ContentView.swift
@@ -9,8 +9,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var rememberMe = false
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            PushButton(title: "Remember Me", isOn: $rememberMe)
+            Text(rememberMe ? "On" : "Off")
+        }
+    }
+}
+
+struct PushButton: View {
+    
+    var title:String
+    @Binding var isOn:Bool
+    
+    var onColor = [Color.orange, Color.pink]
+    var ofColor = [Color.gray, Color.black]
+    
+    var body: some View{
+        Button(title){
+            self.isOn.toggle()
+        }
+        .padding()
+        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColor: ofColor), startPoint: .top, endPoint: .bottom))
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 5)
     }
 }
 
